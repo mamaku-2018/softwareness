fd28471b32c5803ae0b6a4a1efa8c97beb4bfeeb
Caitlyn Bayley <caitlyn.bayley@gmail.com>
2018-07-12 11:30:23 +1200

    createdobject, waiting on action

---
+++ client/components/Company_Add.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Route, Router} from 'react-router'
+import {connect} from 'react-redux'
+
+class CompanyName extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      siteURL: '',
+      nzCompany: 0
+    }
+    this.fillState = this.fillState.bind(this)
+  }
+
+  fillState (e) {
+    switch (e.currentTarget.id) {
+      case 'name':
+        this.setState.name = e.target.value
+        break
+      case 'site-url':
+        this.setState.siteURL = e.target.value
+        break
+      case 'nz-company':
+        this.setState.nzCompany = e.target.value
+        break
+    }
+  }
+
+  addInfo () {
+    this.props.dispatch(/*add*/(this.state))
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Add Company</h2>
+        <h3>Name</h3>
+        <input onClick={this.fillState} type='text' id='name'/>
+        <h3>Site URL</h3>
+        <input onClick={this.fillState} type='text' id='site-url'/>
+        <h3>NZ Company #</h3>
+        <input onClick={this.fillState} type='text' id='nz-company'/>
+        <button className='addButt' onClick={this.addInfo}>ADD</button>
+      </div>
+    )
+  }
+}
+
+export default connect()(CompanyName)
