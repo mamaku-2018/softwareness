eb32cb44dc1a4dedd1019a24d0bcebf6367b38f0
Indigo Phillips <indigogeorgiana@gmail.com>
2018-07-12 16:23:30 +1200

    Corrected all Component names, Reducers, Actions

--- client/components/CompanyInfo.jsx
+++ client/components/CompanyInfo.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {getCompanyInfo} from '../actions'
 
-const companyProfile = ({companyProfile}) => ( // from reducer
-  <div>
-    {companyProfile.name}
-    {companyProfile.website_url}
-    {companyProfile.country_id}
-  </div>
-)
+class CompanyInfo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount () {
+    this.props.dispatch(getCompanyInfo())
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.companyProfile.name}
+        {this.props.companyProfile.website_url}
+        {this.props.companyProfile.country_id}
+      </div>
+
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -17,4 +32,4 @@ const mapStateToProps = (state) => {
 
 export default connect(
   mapStateToProps
-)(companyProfile)
+)(CompanyInfo)
