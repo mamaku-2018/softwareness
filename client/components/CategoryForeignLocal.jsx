9fb2e881dfab151fed0ec275272abcc547183048
User <user@Users-MacBook-Pro.local>
2018-07-13 09:48:38 +1200

    playing with color

--- client/components/CategoryForeignLocal.jsx
+++ client/components/CategoryForeignLocal.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {PieChart, Pie, Tooltip} from 'recharts'
+import {PieChart, Pie, Tooltip, Cell, } from 'recharts'
 
 const data = [{name: 'foreign', value: 40}, {name: 'local', value: 60}]
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+const colors = ['#4df9d1', '#b8f24d']
 
 const localforeign = () => {
   return (
@@ -13,13 +13,21 @@ const localforeign = () => {
         <Pie
           data={data}
           dataKey="value"
-          nameKey="name" 
+          nameKey="name"
           cx="50%"
-          cy="50%" 
-          innerRadius={60}
+          cy="50%"
+          innerRadius={50}
           fill="#70ffc3"
           label
-        />
+        >
+          {
+            data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]}/>
+            ))
+          }
+        </Pie>
+        <legend align='left' />
+
         <Tooltip />
       </PieChart>
     </div>
