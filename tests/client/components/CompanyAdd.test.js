438cb92f2f0afa2b85e683772658f68dd5adfd22
Don Smith <don@antiantonym.com>
2018-07-13 14:25:51 +1200

    improve component test

--- tests/client/components/CompanyAdd.test.js
+++ tests/client/components/CompanyAdd.test.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {configure, shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import {CompanyName} from '../../client/components/CompanyAdd'
+import {CompanyName} from '../../../client/components/CompanyAdd'
 configure({adapter: new Adapter()})
 
 test('<CompanyName />', () => {
-  const expected = 'Add CompanyNameSite URLCompany #ADD'
+  const expected = 'Add Company'
 
   const wrapper = shallow(<CompanyName />)
-  const actual = wrapper.text()
+  const actual = wrapper.find('h2').text()
 
   expect(actual).toEqual(expected)
 })
