9682cd25f304fb8ed5566a9fee1645d3cc29c7fb
anthony-xiao <juniorxiao01@gmail.com>
2018-07-13 17:04:21 +1200

    updated first enzyme test and have stat link to stat page

--- tests/client/components/CategoryMaleFemale.test.js
+++ tests/client/components/CategoryMaleFemale.test.js
@@ -2,19 +2,15 @@ import React from 'react'
 import {render, shallow, configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {CategoryMaleFemale} from '../../../client/components/CategoryMaleFemale'
+import {Stats} from '../../../client/components/Stats'
 
 configure({adapter: new Adapter()})
 
 test('<CategoryMaleFemale>', () => {
-  const expected = 'Gender Split of Each Cateogry'
-  const wrapper = shallow(<CategoryMaleFemale />)
+  const expected = 'Gender Split'
+  const testFn = jest.fn()
+  const wrapper = shallow(<CategoryMaleFemale dispatch={testFn} />)
   const actual = wrapper.find('h2').text()
-  expect(actual).toEqual(expected)
+  expect(actual).toContain(expected)
 })
 
-test('<Stats> returns <CategoryMaleFemale />', () => {
-  const expected = 0
-  const wrapper = render(<CategoryMaleFemale />)
-  const actual = wrapper.find('BarChart').length > 0
-  expect(actual).toBe(expected)
-})
