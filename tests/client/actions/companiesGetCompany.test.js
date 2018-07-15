4b09451e3262e800e7fde83e130cc2d1d90ced83
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-07-16 11:04:28 +1200

    companiesGetCompany tests fixed to correctly check that url peramater is a number

--- tests/client/actions/companiesGetCompany.test.js
+++ tests/client/actions/companiesGetCompany.test.js
@@ -7,7 +7,10 @@ import {SHOW_ERROR} from '../../../client/actions'
 jest.mock('superagent', () => {
   return {
     get: (url) => {
-      if (url.split('/')[1] === 'api') {
+      const splitUrl = url.split('/')
+      const id1 = Number(splitUrl[splitUrl.length - 1])
+      const id2 = Number(splitUrl[splitUrl.length - 1])
+      if (id1 === id2) {
         return Promise.resolve({body: {
           company: {
             name: 'test', websiteUrl: 'www.test.com', countryId: '123'
@@ -22,7 +25,7 @@ jest.mock('superagent', () => {
 
 test('get companies gets the correct actions', () => {
   const dispatch = jest.fn()
-  return getCompanyInfo('/api/v1/companies')(dispatch)
+  return getCompanyInfo(2)(dispatch)
     .then(() => {
       expect(dispatch.mock.calls[0][0].type).toBe(RECEIVE_COMPANY_INFO)
     })
@@ -30,8 +33,8 @@ test('get companies gets the correct actions', () => {
 
 test('get companies returns an error with incorrect url', () => {
   const dispatch = jest.fn()
-  return getCompanyInfo('/companies')(dispatch)
-    .catch(() => {
+  return getCompanyInfo('do words')(dispatch)
+    .then(() => {
       expect(dispatch.mock.calls[0][0].type).toBe(SHOW_ERROR)
     })
 })
