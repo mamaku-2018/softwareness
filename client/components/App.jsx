91b6c19aa068205789d19989c7d95cfd0adedc5d
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-16 11:31:37 +1200

    finding conflict

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Nav from './Nav'
-<<<<<<< HEAD
-import About from './About'
-import Stats from './Stats'
-import Homegraph from './Homegraph'
-import Homebuttons from './Homebuttons'
-import ErrorMessage from './ErrorMessage'
-import WaitIndicator from './WaitIndicator'
-=======
 import Home from './Home'
 import Stats from './Stats'
 import Footer from './Footer'
@@ -18,7 +10,6 @@ import CompanyInfo from './CompanyInfo'
 import ErrorMessage from './ErrorMessage'
 import WaitIndicator from './WaitIndicator'
 import Register from './auth/Register'
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
 
 const App = () => {
   return (
@@ -30,18 +21,11 @@ const App = () => {
         <div className='container'>
           <Route exact path='/' component={Home} />
           <Route path='/stats' component={Stats} />
-<<<<<<< HEAD
-          <Route exact path='/' component={Homegraph} />
-          <Route exact path='/' component={About} />
-          <Route path='/' component={Homebuttons} />
-          <WaitIndicator />
-=======
           <Route path='/register' component={Register} />
           <Switch>
             <Route path='/companies/add' component={CompanyAdd} />
             <Route path='/companies/:id' component={CompanyInfo} />
           </Switch>
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
         </div>
         <Footer />
       </div>
--- client/components/Homebuttons.jsx
+++ client/components/Homebuttons.jsx
@@ -4,14 +4,9 @@ import {Link} from 'react-router-dom'
 const Homebuttons = () => {
   return (
     <div className='homebuttons'>
-<<<<<<< HEAD
-      <p><Link to='/companies/add' className="button">Add company</Link></p>
-      <p><Link to='/companies' className="button">View companies</Link></p>
-=======
       <Link to='/companies/add' className='button'>Add company</Link>
       <Link to='/companies' className='button'>View companies</Link>
       <Link to='/stats' className='button'>Stats</Link>
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
     </div>
   )
 }
