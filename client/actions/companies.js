6704d4de37f5fd85c7cbd783a8d32b3a3f06e099
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-16 11:26:06 +1200

    pulling from dev

--- client/actions/companies.js
+++ client/actions/companies.js
@@ -2,25 +2,12 @@ import request from 'superagent'
 
 import {showError} from './index'
 
-<<<<<<< HEAD
-export const RECEIVE_COMPANY_ID = 'RECEIVE_COMPANY_ID'
-export const RECEIVE_COMPANY = 'RECEIVE_COMPANY'
-export const RECEIVE_COMPANIES = 'RECEIVE_COMPANIES'
-
-export const receiveCompanyId = (id) => {
-  return {
-    type: RECEIVE_COMPANY_ID,
-    id: id
-  }
-}
-=======
 export const REQUEST_COMPANIES = 'REQUEST_COMPANIES'
 export const RECEIVE_COMPANIES = 'RECEIVE_COMPANIES'
 export const REQUEST_COMPANY_INFO = 'REQUEST_COMPANY_INFO'
 export const RECEIVE_COMPANY_INFO = 'RECEIVE_COMPANY_INFO'
 export const REQUEST_COMPANY_PROFILE = 'REQUEST_COMPANY_PROFILE'
 export const RECEIVE_COMPANY_PROFILE = 'RECEIVE_COMPANY_PROFILE'
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
 
 export function addCompany (company) {
   return (dispatch) => {
@@ -28,11 +15,7 @@ export function addCompany (company) {
       .post('/api/v1/companies/add')
       .send(company)
       .then(res => {
-<<<<<<< HEAD
-        dispatch(receiveCompanyId(res.body.newId))
-=======
         dispatch(requestCompanyInfo(res.body.newId))
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
       })
       .catch(err => {
         dispatch(showError(err.message))
@@ -40,16 +23,6 @@ export function addCompany (company) {
   }
 }
 
-<<<<<<< HEAD
-export const receiveCompany = (company) => {
-  return {
-    type: RECEIVE_COMPANY,
-    company: company
-  }
-}
-
-export function getCompany (id) {
-=======
 export const requestCompanyInfo = () => {
   return {
     type: REQUEST_COMPANY_INFO
@@ -64,16 +37,11 @@ export const receiveCompanyInfo = (companyInfo) => {
 }
 
 export function getCompanyInfo (id) {
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
   return (dispatch) => {
     return request
       .get(`/api/v1/companies/${id}`)
       .then(res => {
-<<<<<<< HEAD
-        dispatch(receiveCompany(res.body))
-=======
         dispatch(receiveCompanyInfo(res.body))
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
       })
       .catch(err => {
         dispatch(showError(err.message))
@@ -81,15 +49,12 @@ export function getCompanyInfo (id) {
   }
 }
 
-<<<<<<< HEAD
-=======
 export const requestCompanies = () => {
   return {
     type: REQUEST_COMPANIES
   }
 }
 
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
 export const receiveCompanies = (companies) => {
   return {
     type: RECEIVE_COMPANIES,
@@ -109,8 +74,6 @@ export function getCompanies () {
       })
   }
 }
-<<<<<<< HEAD
-=======
 
 export const requestCompanyProfile = () => {
   return {
@@ -138,4 +101,3 @@ export function getCompanyProfile (id) {
       })
   }
 }
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
