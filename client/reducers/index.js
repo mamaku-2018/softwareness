53376a09e8f12d43ea2fd24699fa73d334eb58e3
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-07-16 11:27:53 +1200

    conflicts

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,20 +4,13 @@ import waiting from './waiting'
 import categoryMaleFemale from './stats/categoryMaleFemale'
 import localForeign from './localForeign'
 import companyInfo from './companyInfo'
-<<<<<<< HEAD
-=======
 import companyProfile from './companyProfile'
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
 import errorMessage from './errorMessage'
 export default combineReducers({
   errorMessage,
   waiting,
   localForeign,
   categoryMaleFemale,
-<<<<<<< HEAD
-  companyInfo
-=======
   companyInfo,
   companyProfile
->>>>>>> 51f2b2650a489ca35b3c84a80d215c97758c6fed
 })
