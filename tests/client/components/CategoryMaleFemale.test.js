5b0d20d6556bf6a14f9bf817aae46823ea15da99
anthony-xiao <juniorxiao01@gmail.com>
2018-07-16 17:22:41 +1200

    made suggested adjustments to empty function for dispatch

--- tests/client/components/CategoryMaleFemale.test.js
+++ tests/client/components/CategoryMaleFemale.test.js
@@ -8,32 +8,29 @@ configure({adapter: new Adapter()})
 
 test('<CategoryMaleFemale> includes correct text', () => {
   const expected = 'Gender Split'
-  const testFn = jest.fn()
-  const wrapper = shallow(<CategoryMaleFemale dispatch={testFn} />)
+  const wrapper = shallow(<CategoryMaleFemale dispatch={() => {}} />)
   const actual = wrapper.find('h2').text()
   expect(actual).toContain(expected)
 })
 
 test('<CategoryMaleFemale> is including props passed in', () => {
   const gender = [{Male: 3, Female: 10, Category: 'creative'}]
-  const testFn = jest.fn()
-  const wrapper = mount(<CategoryMaleFemale gender={gender} dispatch={testFn} />)
+  const wrapper = mount(<CategoryMaleFemale gender={gender} dispatch={() => {}} />)
   const actual = wrapper.find('BarChart').props().data
   expect(actual).toBe(gender)
 })
 
 test('<CategoryLocalForeign> includes correct text', () => {
   const expected = '<PieChart />'
-  const testFn = jest.fn()
-  const wrapper = shallow(<CategoryForeignLocal dispatch={testFn} local={[]} />)
+  const wrapper = shallow(<CategoryForeignLocal dispatch={() => {}} local={[]} />)
   const actual = wrapper.find('PieChart').text()
   expect(actual).toContain(expected)
 })
 
 test('<CategoryLocalForeign> is including props passed in', () => {
   const local = [{name: 'Local', value: 10}, {name: 'Foreign', value: 20}]
-  const testFn = jest.fn()
-  const wrapper = mount(<CategoryForeignLocal local={local} dispatch={testFn} />)
+  // const testFn = jest.fn()
+  const wrapper = mount(<CategoryForeignLocal local={local} dispatch={() => {}} />)
   const actual = wrapper.find('Pie').props().data
   expect(actual.map(data => data.payload)).toEqual(local)
 })
