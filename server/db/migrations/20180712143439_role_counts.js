4886c353d8651a288905549cc5e5f792d056ae37
anthony-xiao <juniorxiao01@gmail.com>
2018-07-17 16:37:50 +1200

    removed reference key to allow for postgres

--- server/db/migrations/20180712143439_role_counts.js
+++ server/db/migrations/20180712143439_role_counts.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('role_counts', table => {
     table.increments('id').primary()
-    table.integer('role_id').references('roles.id')
-    table.integer('company_id').references('companies.id')
+    table.integer('role_id')
+    table.integer('company_id')
     table.integer('count')
     table.date('timestamp')
     table.integer('avg_years_exp')
